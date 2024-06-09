b354d8d84259ea5545b5f253c6b69b6135c0685f
Hafid <hafid@gmail.com>
2024-06-09 22:55:11 +0700

    fix bug

--- alternatif-ubah.php
+++ alternatif-ubah.php
@@ -10,31 +10,31 @@ $eks->id = $id;
 
 $eks->readOne();
 
-if($_POST){
+if ($_POST) {
 
 	$eks->nm = $_POST['nm'];
-	$esk->id = $_POST['id'];
-	
-	if($eks->update()){
+	$eks->id = $_POST['id'];
+
+	if ($eks->update()) {
 		echo "<script>location.href='alternatif.php'</script>";
-	} else{
-?>
-<div class="alert alert-danger alert-dismissible" role="alert">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  <strong>Gagal Ubah Data!</strong> Terjadi kesalahan, coba sekali lagi.
-</div>
-<?php
+	} else {
+		?>
+		<div class="alert alert-danger alert-dismissible" role="alert">
+		  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		  <strong>Gagal Ubah Data!</strong> Terjadi kesalahan, coba sekali lagi.
+		</div>
+		<?php
 	}
 }
 ?>
 		<div class="row">
 		  <div class="col-xs-12 col-sm-12 col-md-8">
-		  	<div class="page-header">
+			  <div class="page-header">
 			  <h5>Ubah Alternatif</h5>
 			</div>
 				  <div class="form-group">
-				    <label for="nm">Nama Alternatif</label>
-				    <input type="text" class="form-control" id="nm" name="nm" value="<?php echo $eks->nm; ?>">
+					<label for="nm">Nama Alternatif</label>
+					<input type="text" class="form-control" id="nm" name="nm" value="<?php echo $eks->nm; ?>">
 				  </div>
 				  <button type="submit" class="btn btn-primary">Ubah</button>
 				  <button type="button" onclick="location.href='alternatif.php'" class="btn btn-success">Kembali</button>
@@ -42,9 +42,9 @@ if($_POST){
 			  
 		  </div>
 		  <div class="col-xs-12 col-sm-12 col-md-4">
-		  	<?php include_once 'sidebar.php'; ?>
+			  <?php include_once 'sidebar.php'; ?>
 		  </div>
 		</div>
 		<?php
-include_once 'footer.php';
-?>
+		include_once 'footer.php';
+		?>
